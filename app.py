cfdf06cd0d4e091875294894553fffc86be99bc7
Sree Raja <SRaja001@gmail.com>
2013-03-06 02:46:08 +0100

    almost there

--- app.py
+++ app.py
@@ -1,23 +1,30 @@
-import os
+from __future__ import with_statement
 from sqlite3 import dbapi2 as sqlite3
 from flask import Flask, jsonify, request, render_template,session, url_for, abort, \
      render_template, flash, _app_ctx_stack
 from wtforms import Form, TextField, validators
+from contextlib import closing
+
 
 DATABASE = "/tmp/flaskr.db"
+SECRET_KEY = 'development key'
+DEBUG = True
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
 
 def init_db():
-    with app.app_contect():
-        db = get_db()
+    with closing(connect_db()) as db:
         with app.open_resource('schema.sql') as f:
             db.cursor().executescript(f.read())
         db.commit()
 
 def get_db():
-    top = app_ctx_stack.top
+    top = _app_ctx_stack.top
     if not hasattr(top, 'sqlite_db'):
         sqlite_db = sqlite3.connect(app.config['DATABASE'])
         sqlite_db.row_factory = sqlite3.Row
@@ -32,8 +39,7 @@ def close_db_connection(exception):
         top.sqlite_db.close()
 
 class RegistrationForm(Form):
-    email = TextField('Email Address',
-            validators.Email(message='Not a valid Email'))
+    email = TextField('Email Address',[validators.Email('Not a valid Email')])
 
 
 @app.route('/')
@@ -47,13 +53,13 @@ def save_email():
     if request.method == 'POST' and form.validate():
         data = form.email.data
         db = get_db()
-        db.execute('insert into entries (email) values (?)',
-                     [request.form['email']])
+        db.execute('insert into entries (email) values (?)',[request.form['email']])
         db.commit()
-        return "Please provide a valid email."
-    return "Thanks for your e-mail!"
+        return "Thanks for your e-mail."
+    return "Please provide a valid Email."
 
 
 if __name__ == '__main__':
+    init_db()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
