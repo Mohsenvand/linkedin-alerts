86c9ed5b3e51799147ab580d47c3e27af9730d27
Praful Mathur <praful.mathur@gmail.com>
2013-03-07 12:58:18 -0500

    Send everything to emailbin.derivatived.com

--- app.py
+++ app.py
@@ -1,66 +1,12 @@
 from __future__ import with_statement
 import os
-from sqlite3 import dbapi2 as sqlite3
-from flask import (
-    Flask,
-    request,
-    render_template,
-    #jsonify,
-    #session,
-    #url_for,
-    #abort,
-    #flash,
-    _app_ctx_stack,
-)
-from flask.ext.basicauth import BasicAuth
-from wtforms import Form, TextField, validators
-from contextlib import closing
+from flask import Flask, render_template
 
-
-DATABASE = "./flaskr.db"
-SECRET_KEY = 'development key'
-DEBUG = True
+DEBUG = False
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
-
-app.config['BASIC_AUTH_USERNAME'] = os.environ.get('AUTH_USERNAME', None)
-app.config['BASIC_AUTH_PASSWORD'] = os.environ.get('AUTH_PASSWORD', None)
-
-basic_auth = BasicAuth(app)
-
-
-def connect_db():
-    return sqlite3.connect(app.config['DATABASE'])
-
-
-def init_db():
-    with closing(connect_db()) as db:
-        with app.open_resource('schema.sql') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-
-
-def get_db():
-    top = _app_ctx_stack.top
-    if not hasattr(top, 'sqlite_db'):
-        sqlite_db = sqlite3.connect(app.config['DATABASE'])
-        sqlite_db.row_factory = sqlite3.Row
-        top.sqlite_db = sqlite_db
-
-    return top.sqlite_db
-
-
-@app.teardown_appcontext
-def close_db_connection(exception):
-    top = _app_ctx_stack.top
-    if hasattr(top, 'sqlite_db'):
-        top.sqlite_db.close()
-
-
-class RegistrationForm(Form):
-    email = TextField('Email Address', [validators.Email('Not a valid Email')])
 
 
 @app.route('/')
@@ -68,29 +14,6 @@ def hello():
     return render_template('index.html')
 
 
-@app.route('/email',  methods=['POST'])
-def save_email():
-    form = RegistrationForm(request.form)
-    if request.method == 'POST' and form.validate():
-        email = request.form['email']
-        db = get_db()
-        db.execute('insert into entries (email) values (?)', [email])
-        db.commit()
-        return "Thanks for your e-mail."
-    return "Please provide a valid Email."
-
-
-@app.route('/_get_emails',  methods=['GET'])
-@basic_auth.required
-def _get_emails():
-    db = get_db()
-    c = db.cursor()
-    c.execute('SELECT * FROM entries;')
-    row = c.fetchall()
-    return '<br />'.join([x[1] for x in row])
-
-
 if __name__ == '__main__':
-    init_db()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
